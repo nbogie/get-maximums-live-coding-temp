d3999280a838ffa26d2cfb7bf6c37313b8d5bd36
nbogie <neillbogie@googlemail.com>
2022-10-25 16:07:17 +0100

    add initial skeleton function with decent test cases

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-//Import functions and constants from modules in "common-js" format.
-//This is outdated but still common.  Not used in TypeScript.
-const { sum } = require("./sum");
+/**
+ * Returns an array of the maximum numbers from each of the subarrays of the given array.
+ * 
+ * function: getMaximums 
+ * params: 
+ *   inputArrays: number[][]
+ * returns:
+ *   array of numbers (maximums from each subarray in the input array)
+ * 
+*/
+function getMaximums(inputArrays) {
+    
+}
 
-console.log("sum is ", sum(10, 20));
-console.log("This is main.js.");
-console.log("You should probably use `yarn test` or `yarn test --watchAll`");
+console.log(getMaximums([]), [])
+console.log(getMaximums([ [20] ]), [20])
+console.log(getMaximums([ [-1000, -2000] ]), [-1000])
+console.log(getMaximums([ [2, 3.14, 1], [100, 500, 200] ]), [3.14, 500])
+console.log(getMaximums([[10, 70, 30], [40], [20, -30, 80]]), [70, 40, 80])
+console.log(getMaximums([[10],[10,3,3],[-1,-50]]), [10, 10, -1])
+console.log(getMaximums([[1,2,3,4], [3,5,2], [100,-1,4]]), [4,5,100])
